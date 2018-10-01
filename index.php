c223b3fc2ea30ccd83082e77eb172ee3d06c4e39
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-10-01 14:17:01 +0200

    dcdate reparse

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ libxml_use_internal_errors(true);
 $feed_url = "https://nachrichtenfeeds.br.de/rdf/boards/QXAPkQJ";
 $content = file_get_contents($feed_url);
 $content = preg_replace('/&(?!;{6})/', '&amp;', $content);
-$content = preg_replace('/dc:date/', 'dcdate;', $content);
+$content = preg_replace('dc:date', 'dcdate', $content);
 $xml = simplexml_load_string($content);
 
 if ($xml === false) {
@@ -37,7 +37,6 @@ if ($xml === false) {
     <h1>BR 25</h1>
     <ul>
       <?php foreach($xml->item as $entry): ?>
-      <pre><?php var_dump($entry);?></pre>
       <li>
         <a href='<?php echo $entry->link;?>' title='<?php echo $entry->title;?>' target="_blank">
           [
