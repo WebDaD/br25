2190f488358e30e78461a3ac3b72c32ad434aa1d
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-10-01 13:26:59 +0200

    container added

--- index.php
+++ index.php
@@ -15,26 +15,36 @@ $xml = new SimpleXmlElement($content);
 
 ?>
 <html>
-  <head>
-    <title>BR 25</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  </head>
-  <body>
+
+<head>
+  <title>BR 25</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+    crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container">
     <h1>BR 25</h1>
     <ul>
       <?php foreach($xml->channel->item as $entry): ?>
-        <li>
-          <a href='<?php echo $entry->link;?>' title='<?php echo $entry->title;?>' target="_blank">
-            [<?php echo $entry["dc:date"];?>] <?php echo $entry->title;?>
-          </a>
-          <br/>
-          <p><?php echo $entry->description;?></p>
-        </li>
+      <li>
+        <a href='<?php echo $entry->link;?>' title='<?php echo $entry->title;?>' target="_blank">
+          [
+          <?php echo $entry["dc:date"];?>]
+          <?php echo $entry->title;?>
+        </a>
+        <br />
+        <p>
+          <?php echo $entry->description;?>
+        </p>
+      </li>
       <?php endforeach;?>
     </ul>
-    <hr/>
+    <hr />
     <p>
       <a href="<?php echo $feed_url;?>">RSS-Feed</a>
     </p>
-  </body>
+  </div>
+</body>
+
 </html>
