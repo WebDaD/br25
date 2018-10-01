c83bb41f3a12f0d1a413cca49a633dd70676e489
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-10-01 13:24:23 +0200

    Add Errors

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $feed_url = "https://nachrichtenfeeds.br.de/rdf/boards/QXAPkQJ";
 $content = file_get_contents($feed_url);
 $xml = new SimpleXmlElement($content);
