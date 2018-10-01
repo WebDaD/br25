b6a7f7930fe8f3d37dec06f34945ae3bbc3ab45e
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-10-01 14:37:50 +0200

    boostrap deps

--- index.php
+++ index.php
@@ -63,16 +63,22 @@ if ($xml === false) {
         <div class="card">
           <img class="card-img-top" src="<?php echo $entry->mpimage[0]->mpdata;?>" alt="<?php echo $entry->mpimage[0]->mpalt;?>">
           <div class="card-body">
-            <h5 class="card-title"><?php echo $entry->title;?></h5>
-            <p class="card-text"><?php echo $entry->description;?></p>
+            <h5 class="card-title">
+              <?php echo $entry->title;?>
+            </h5>
+            <p class="card-text">
+              <?php echo $entry->description;?>
+            </p>
           </div>
           <div class="card-body">
-          <a class="card-link" href='<?php echo $entry->link;?>' title='<?php echo $entry->title;?>' target="_blank">
+            <a class="card-link" href='<?php echo $entry->link;?>' title='<?php echo $entry->title;?>' target="_blank">
               Weiterlesen ...
             </a>
           </div>
           <div class="card-footer">
-            <small class="text-muted"><?php echo date_format(date_create($entry->dcdate), "d.m.Y H:i");?></small>
+            <small class="text-muted">
+              <?php echo date_format(date_create($entry->dcdate), "d.m.Y H:i");?>
+            </small>
           </div>
         </div>
         <?php endforeach; ?>
@@ -83,7 +89,12 @@ if ($xml === false) {
         <a href="<?php echo $feed_url;?>">RSS-Feed</a>
       </p>
     </div>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+      crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+      crossorigin="anonymous"></script>
   </body>
 
   </html>
